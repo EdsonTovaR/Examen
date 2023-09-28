35810b98af3a697484b80bd09932a7bb262b03e3
Edson Tovar <102639181+EdsonTovaR@users.noreply.github.com>
2023-09-27 23:34:14 -0600

    .

---
+++ biblioteca.py
@@ -0,0 +1,48 @@
+def es_primo(num):
+    if num <= 1:
+        return False
+    if num <=3:
+        return True
+    if num % 2==0  or num % 3 == 0:
+        return False
+    
+    i=5
+    while i*i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        
+        i+=6
+    return True
+
+
+
+def nextPrime(n):
+    if n<=1:
+        n=2
+    else:
+        n+=1
+
+    while True:
+        if es_primo(n):
+            return n
+        n+=1
+
+def val_mediana(n1,n2,n3):
+    numeros=[n1,n2,n3]
+    numeros.sort()
+
+    return numeros[1]
+
+import random
+import string
+
+def generar_contrasena():
+    longitud = random.randint(7, 10)  
+    contrasena = ''.join(random.choice(string.printable[33:127]) for _ in range(longitud))
+    return contrasena
+
+import math
+
+def calcular_hipotenusa(a, b):
+    hipotenusa = math.sqrt(a**2 + b**2)
+    return hipotenusa
